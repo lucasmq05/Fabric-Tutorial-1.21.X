6af36c094055ffdf9d234d074790434c76b60403
Lucas <lucasqueirozbr100@gmail.com>
2026-02-16 17:35:42 -0300

    commit inicial

---
+++ src/main/java/net/luvas/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.luvas.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
