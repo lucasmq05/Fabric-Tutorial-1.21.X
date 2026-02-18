329581e118533c814fe33a7cd27825718a2a13e7
Lucas <lucasqueirozbr100@gmail.com>
2026-02-18 20:42:06 -0300

    added pink garnet and raw pink garnet blocks

--- src/main/java/net/luvas/tutorialmod/TutorialMod.java
+++ src/main/java/net/luvas/tutorialmod/TutorialMod.java
@@ -3,6 +3,7 @@ package net.luvas.tutorialmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.luvas.tutorialmod.block.ModBlocks;
+import net.luvas.tutorialmod.item.ModItemGroups;
 import net.luvas.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +15,8 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 	}
